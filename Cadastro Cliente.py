341c4128af6076e9ad718ada7b3a78478b2635eb
MSantos2022 <102885229+MSantos2022@users.noreply.github.com>
2022-05-22 15:16:52 -0300

    Add files via upload

---
+++ Cadastro Cliente.py
@@ -0,0 +1,32 @@
+print(" ---------- CADASTRO DE CLIENTES PARA VENDA DE MOTOCICLETAS ---------")
+print(" --------------LOJAS MULTVERSOS MOTOR -------------")
+
+
+nome = input("Digite o nome do Cliente:")
+cpf = input("Digit o número do cpf:")
+datanasc = input("Digite a data de nascimento:")
+idade = input("Digite a sua idade:")
+endereco = input("Digite o endereço atual:")
+fone = input("Dugite o telefone atual:")
+renda = input("Digite o valor da sua renda mensal:")
+
+print("Cliente:{}\ncpf:{}\nData de nascimento:{}\nidade:{}\nEndereço:{}\nfone:{}\nrenda{}\n".format(nome,cpf,datanasc,idade,endereco,fone,renda))
+
+print("_________CADASTRO FINALIZADO COM SUCESSO________")
+print(" +++++++++ LOJAS MULTVERSOS MOTOR AGRADECE PELA SEU CADASTRO ++++++++")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
